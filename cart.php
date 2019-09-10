5bb5f1ca88859f9dd6145d8f37c2b6a5d9076f5f
AurelienBarbier <aurelien@wildcodeschool.fr>
2019-09-10 17:40:02 +0200

    edit challenge and dynamic

--- cart.php
+++ cart.php
@@ -1,7 +1,7 @@
 <?php require 'inc/head.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
-      TODO : Display shopping cart items from $_SESSION here.
+      TODO : Display shopping cart items from $_COOKIES here.
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
