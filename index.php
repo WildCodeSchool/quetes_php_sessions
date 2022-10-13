dd0f1b9a6d210692023c948781f0470f717714d4
Bokassa jean le Grand <jean-le-grand.bokassa@wildcodeschool.com>
2022-10-13 07:20:31 +0200

    changed images path

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <?php foreach ($catalog as $id => $cookie) { ?>
             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <figure class="thumbnail text-center">
-                    <img src="assets/img/product-<?= $id; ?>.jpg" alt="<?= $cookie['name']; ?>" class="img-responsive">
+                    <img src="assets/img/product-<?= $id; ?>.png" alt="<?= $cookie['name']; ?>" class="img-responsive">
                     <figcaption class="caption">
                         <h3><?= $cookie['name']; ?></h3>
                         <p><?= $cookie['description']; ?></p>
